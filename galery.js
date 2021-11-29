a94cd2210056f4ae90dd66c1ea011b861ffc6714
AlexAR93 <88462330+AlexAR93@users.noreply.github.com>
2021-11-29 17:33:29 -0300

    Add files via upload

--- galery.js
+++ galery.js
@@ -23,9 +23,17 @@ openWindowWhatsapp();
 
 let iAbout=document.getElementById("about")
 let p=document.querySelector(".hide-about")
+let extend=document.querySelector(".extend")
 let about=()=>{
     iAbout.addEventListener("click",()=>{
         p.classList.toggle("hide-about")
+        if(extend.innerHTML=="ver info"){
+            extend.innerHTML="esconder"
+        }else if(extend.innerHTML=="esconder"){
+            extend.innerHTML="ver info"
+        }else{
+            null
+        }
 })
 }
 about()
